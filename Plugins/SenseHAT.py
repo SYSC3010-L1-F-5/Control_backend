63b880d63bdac31a9633c3b0f33c8d36426d43b5
Haoyu Xu <i@halyul.dev>
2020-09-21 19:25:00 -0400

    [Haoyu Xu] feat: add basic plugin operations

---
+++ Plugins/SenseHAT.py
@@ -0,0 +1,12 @@
+"""
+
+    SenseHAT for quick status check
+
+"""
+
+from sense_hat import SenseHat
+
+class SenseHAT:
+
+    def __init__(self):
+        self.sense = SenseHat()
---
+++ lib/Plugin.py
@@ -0,0 +1,11 @@
+"""
+
+    All plugin related methods will be here
+    Author: Haoyu Xu
+
+"""
+
+class Plugin:
+
+    def __init__(self):
+        self.debug = False
